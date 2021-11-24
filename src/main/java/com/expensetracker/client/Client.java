cedbbc12df8092811d6289c73b6cd4f9642d87b2
nayeemahmedshaik <sheiknayeemahmed@gmail.com>
2021-11-24 12:33:54 +0530

    fixed loop

--- src/main/java/com/expensetracker/client/Client.java
+++ src/main/java/com/expensetracker/client/Client.java
@@ -16,9 +16,110 @@ import com.expensetracker.service.UserServiceImpl;
 public class Client {
 	static Scanner scanner = new Scanner(System.in);
 
+	static IUserService userService = new UserServiceImpl();
+	static IExpenseService expenseService = new ExpenseServiceImpl();
+	static User foundUser = null;
+	static int count = 0;
+
+	static void expenseFunctions() {
+		System.out.println();
+		System.out.println("Add Transaction PRESS 1");
+		System.out.println("Update Transaction PRESS 2");
+		System.out.println("Delete Transaction PRESS 3");
+		System.out.println("Get Transaction By Date PRESS 4");
+		System.out.println("Get Transaction By User PRESS 5");
+		System.out.println("Get Category statistics PRESS 6");
+		System.out.println("Get mode of transaction stats By  PRESS 7");
+		System.out.println("Logout PRESS 8");
+
+		int input = scanner.nextInt();
+		switch (input) {
+		case 1:
+			System.out.println("Enter type(credit/debit)");
+			String type = scanner.next();
+
+			System.out.println("Enter category(apparel/groceries/food)");
+			String category = scanner.next();
+
+			System.out.println("Enter mode of transaction(upi/netbanking/wiretransfer/card)");
+			String mode = scanner.next();
+
+			System.out.println("Enter amount spent");
+			double amount = scanner.nextDouble();
+
+			User userExpense = new User(foundUser.getUserid());
+			Expense expense = new Expense(userExpense, type, category, mode, amount);
+			expenseService.addTransaction(expense);
+
+			break;
+		case 2:
+			System.out.println("Enter transaction Id : ");
+			int transactionId = scanner.nextInt();
+			System.out.println("Enter category fro update : ");
+			String updateCategory = scanner.next();
+			System.out.println("Enter amount for update : ");
+			double updateAmount = scanner.nextDouble();
+
+			Expense newExpense = new Expense();
+			newExpense.setId(transactionId);
+			newExpense.setCategory(updateCategory);
+			newExpense.setAmount(updateAmount);
+
+			expenseService.updateTransaction(newExpense);
+			break;
+		case 3:
+			System.out.println("Enter transaction Id to delete :");
+			int transactionIdForDelete = scanner.nextInt();
+
+			expenseService.deleteTransaction(transactionIdForDelete);
+			break;
+		case 4:
+			System.out.println("Enter transaction date(yyyy-MM-dd) : ");
+			LocalDate date = LocalDate.parse(scanner.next());
+			try {
+				System.out.println(expenseService.getTransactionByDate(date, foundUser.getUserid()));
+			} catch (ExpenseRecordNotFoundException e) {
+				System.out.println("No transaction found for the specified user and date");
+			}
+			break;
+		case 5:
+			try {
+				System.out.println(expenseService.findTransactionByUser(foundUser.getUserid()));
+			} catch (UserNotFoundException e) {
+				System.out.println("No transaction found for the specified user");
+			}
+			break;
+		case 6:
+			try {
+				List<Expense> categoryExpense = expenseService.getStatsByCategory(foundUser.getUserid());
+				for (Expense expense1 : categoryExpense) {
+					System.out.println(expense1.categoryDisplay());
+				}
+			} catch (UserNotFoundException e) {
+				System.out.println("No records found for the specified user");
+			}
+			break;
+		case 7:
+			try {
+				List<Expense> expenses = expenseService.getStatsByModeOfTransaction(foundUser.getUserid());
+
+				for (Expense expense2 : expenses) {
+					System.out.println(expense2.display());
+				}
+			} catch (UserNotFoundException e) {
+				System.out.println(e.getMessage());
+			}
+			break;
+		case 8:
+			count = 1;
+			System.exit(0);
+		default:
+			break;
+		}
+
+	}
+
 	public static void main(String[] args) {
-		IUserService userService = new UserServiceImpl();
-		IExpenseService expenseService = new ExpenseServiceImpl();
 
 		System.out.println("My Expense Tracker");
 		System.out.println();
@@ -27,10 +128,11 @@ public class Client {
 		System.out.println("-----------------------------");
 
 		while (true) {
-			User foundUser = null;
+
 			System.out.println();
 			System.out.println("Login PRESS 1");
 			System.out.println("SignUp PRESS 2");
+			System.out.println("Press any key to exit");
 			int login = scanner.nextInt();
 
 			switch (login) {
@@ -52,99 +154,8 @@ public class Client {
 				System.out.println();
 				System.out.println();
 				System.out.println("Welcome " + user.getEmail());
-
-				System.out.println();
-				System.out.println("Add Transaction PRESS 1");
-				System.out.println("Update Transaction PRESS 2");
-				System.out.println("Delete Transaction PRESS 3");
-				System.out.println("Get Transaction By Date PRESS 4");
-				System.out.println("Get Transaction By User PRESS 5");
-				System.out.println("Get Category statistics PRESS 6");
-				System.out.println("Get mode of transaction stats By  PRESS 7");
-
-				int input = scanner.nextInt();
-
-				switch (input) {
-				case 1:
-					System.out.println("Enter type(credit/debit)");
-					String type = scanner.next();
-
-					System.out.println("Enter category(apparel/groceries/food)");
-					String category = scanner.next();
-
-					System.out.println("Enter mode of transaction(upi/netbanking/wiretransfer/card)");
-					String mode = scanner.next();
-
-					System.out.println("Enter amount spent");
-					double amount = scanner.nextDouble();
-
-					User userExpense = new User(foundUser.getUserid());
-					Expense expense = new Expense(userExpense, type, category, mode, amount);
-					expenseService.addTransaction(expense);
-
-					break;
-				case 2:
-					System.out.println("Enter transaction Id : ");
-					int transactionId = scanner.nextInt();
-					System.out.println("Enter category fro update : ");
-					String updateCategory = scanner.next();
-					System.out.println("Enter amount for update : ");
-					double updateAmount = scanner.nextDouble();
-
-					Expense newExpense = new Expense();
-					newExpense.setId(transactionId);
-					newExpense.setCategory(updateCategory);
-					newExpense.setAmount(updateAmount);
-
-					expenseService.updateTransaction(newExpense);
-					break;
-				case 3:
-					System.out.println("Enter transaction Id to delete :");
-					int transactionIdForDelete = scanner.nextInt();
-
-					expenseService.deleteTransaction(transactionIdForDelete);
-					break;
-				case 4:
-					System.out.println("Enter transaction date(yyyy-MM-dd) : ");
-					LocalDate date = LocalDate.parse(scanner.next());
-					try {
-						System.out.println(expenseService.getTransactionByDate(date, foundUser.getUserid()));
-					} catch (ExpenseRecordNotFoundException e) {
-						System.out.println("No transaction found for the specified user and date");
-					}
-					break;
-				case 5:
-					try {
-						System.out.println(expenseService.findTransactionByUser(foundUser.getUserid()));
-					} catch (UserNotFoundException e) {
-						System.out.println("No transaction found for the specified user");
-					}
-					break;
-				case 6:
-					try {
-						List<Expense> categoryExpense = expenseService.getStatsByCategory(foundUser.getUserid());
-						for (Expense expense1 : categoryExpense) {
-							System.out.println(expense1.categoryDisplay());
-						}
-					} catch (UserNotFoundException e) {
-						System.out.println("No records found for the specified user");
-					}
-					break;
-				case 7:
-					try {
-						List<Expense> expenses = expenseService.getStatsByModeOfTransaction(foundUser.getUserid());
-
-						for (Expense expense2 : expenses) {
-							System.out.println(expense2.display());
-						}
-					} catch (UserNotFoundException e) {
-						System.out.println(e.getMessage());
-					}
-					break;
-
-				default:
-
-					break;
+				while (count == 0) {
+					expenseFunctions();
 				}
 
 			case 2: {
